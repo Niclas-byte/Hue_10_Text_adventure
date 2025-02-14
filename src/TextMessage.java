d69075ac7374cab53947070598e32fadba64b2c2
Niclas Huemer <niclashuemer@gmail.com>
2025-02-14 10:47:00 +0100

    added class TextMessage

---
+++ src/TextMessage.java
@@ -0,0 +1,8 @@
+public class TextMessage {
+    private void startGame(){
+        System.out.println("""
+                Du wachst in der Nacht in eine 
+                
+                """);
+    }
+}
