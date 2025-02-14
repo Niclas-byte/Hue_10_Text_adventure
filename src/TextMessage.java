628316f43cdae9d1340cb76c3e5009dbbba0b952
Niclas Huemer <niclashuemer@gmail.com>
2025-02-14 14:25:34 +0100

    added method entranceHall and library

--- src/TextMessage.java
+++ src/TextMessage.java
@@ -1,8 +1,29 @@
 public class TextMessage {
     private void startGame(){
         System.out.println("""
-                Du wachst in der Nacht in eine 
-                
+                Du bist in einer verlassenen Villa gefangen und musst einen Weg hinausfinden.
                 """);
     }
+    private void entranceHall(){
+        System.out.println("""
+                Sie sind gerade in der Eingangshalle. Hier befindet sich
+                eine alte Standuhr und eine alte Truhe.
+                Was möchtest du machen?
+                w....nach Norden
+                a....nach Westen
+                s....nach Süden
+                d....nach Osten
+                u....untersuchen
+                """);
+    }
+     private void library(){
+         System.out.println("""
+                 Hier befindet sich die Küche. In der Küche schaust du dich ein
+                 wenig um und findest einen Herd, ein Kochbuch und ein Messer in
+                 einer Messerhalterung.
+                 Was tust du?
+                 a....nach Westen
+                 u....untersuchen
+                 """);
+     }
 }
